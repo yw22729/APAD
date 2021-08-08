5fc1ac15945034aa4f124870f392cbf009bed317
yw22729 <wangyang2018aucta@hotmail.com>
2021-08-08 13:31:06 -0500

    add clustering and hover for map

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ from flask_googlemaps import Map
 db = MongoEngine()
 def create_app(config=None):
     app = Flask(__name__)
-    
+
     if config is not None:
         app.config.from_object(config)
 
@@ -20,7 +20,7 @@ def create_app(config=None):
     from study.views import study_page
     app.register_blueprint(study_page, url_prefix="/study")
 
-    
+
     @app.route('/')
     def home():
         return redirect(url_for('study_page.search'))
@@ -32,26 +32,23 @@ def create_app(config=None):
         studies = Study.objects
         data = []
         for study in studies:
-            data.append({'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png', 
-            'lng': study.location["coordinates"][0], 
-            'lat': study.location["coordinates"][1], 
+            data.append({'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+            'lng': study.location["coordinates"][0],
+            'lat': study.location["coordinates"][1],
             'infobox': f"<img width=100px height=100px src='{study.study_photo}'/>" })
         # creating a map in the view
-        print(data)
-        mymap = Map(
-            identifier="view-side",
-            lat=37.4419,
-            lng=-122.1419,
-            markers=[(37.4419, -122.1419), (37.4300, -122.1400)]
-        )
+        # print(data)
         sndmap = Map(
             identifier="sndmap",
             lat=30.2880433,
             lng=-97.7308176,
             markers= data,
             style="height:80%;width:100%;",
-            zoom=12
+            zoom=12,
+            cluster=True,
+            cluster_imagepath="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"
+
 
         )
-        return render_template('map.html', mymap=mymap, sndmap=sndmap,data=data)
+        return render_template('map.html', sndmap=sndmap,data=data)
     return app
